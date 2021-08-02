92ed0b68c71b13d1310775e915648e04395912af
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-08-02 18:34:05 +0000

    d

--- webpack.config.js
+++ webpack.config.js
@@ -20,19 +20,5 @@ module.exports = {
             }
           }
         ]
-    }
-    
-    /*module:{
-        rules:[
-            {test:/\.(js|jsx)$/},
-            {exclude: /node_modules/},
-            {use:{
-                {loader:"babel-loader"},
-                {options: {
-                    presets: ['@babel/preset-env']
-                }}
-            }
-        ]
-        
-    }*/
+    }    
 }
