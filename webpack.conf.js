2abe7efca3e0cdc7d96da60611cfae6e707ae4c9
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-08-02 15:14:36 +0000

    ReactAplicado

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,9 +1,19 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/test.js',
+    mode: 'production',
+    entry: './src/index.js',
     output: {
         path : path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module:{
+        rules:{
+            test:/\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use:{
+                loader:"babel-loader"
+            }
+        }
     }
-};
+}
