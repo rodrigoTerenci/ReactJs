3fe942abfa6653658d7e67d45757ed70ddf59df6
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-08-05 15:56:50 +0000

    webpack build done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ReactJs
 ReactJs - For Study and Learn
+
+https://github.com/babel/babel-loader/issues/789
--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,21 @@ module.exports = {
           {
             test: /\.m?js$/,
             exclude: /(node_modules|bower_components)/,
-            use: [
-              {loader: 'babel-loader'},
-              {options: {
-                presets: ['@babel/preset-env']
-              }}
-            ]
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env',
+                          // "@babel/preset-react",                                
+                ]
+              }
+            }
           }
         ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
     },
     plugins:[
         new HtmlWebPackPlugins({
