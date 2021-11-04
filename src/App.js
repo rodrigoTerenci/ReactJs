347ee313075c2bca581eb5aed11a0e8528dee9a1
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-11-04 23:17:29 +0000

    learn about statefull(hooks)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React from 'react'
-// import Button from './Button'
+import React, {useState} from 'react'
 
-const buttonA = <button>Historico do Clientes</button>
-
-const buttonB = <button>Cadastrar Cliente  </button>
-
-const hasCustumer = false
 const App = () =>{
+   // let hasCustumer = false
+    const [items, setItems] = useState(['Eu','sei','eu','sei'])
+    
+    const addItem = (item) =>{
+        setItems([...items,item])
+    }
+    const removeItem = (item) =>{
+        setItems([...items.slice(1)])
+    }
     const renderShowHistory = (
         <div> 
-             Clique no botão abaixo para visualizar o hitórico dos clientes
-             <br/>
-             {buttonA}
+             <p>Lista de Clientes</p>
+             <ul>
+                 {items.map(item =><li>{item}</li>)}
+             </ul>
          </div>
     )
     const renderAddCustomer = (
         <div>
             Clique aqui para cadastra um novo Cliente
             <br/>
-            {buttonB}
+            <button onClick={() => addItem('paranoia')}>+</button>
         </div>
     )
     return(
         <div>
             <p>Rodriguinho WeB</p>
             <h2>Desenvolvimento Divertido</h2>
-            {/*renderAddCustomer*/}
-            {hasCustumer ? renderShowHistory : renderAddCustomer}
-            {/* <Button /> */}
+            
+            {renderShowHistory} 
+            {renderAddCustomer}
+            
         </div>
     )
 };
