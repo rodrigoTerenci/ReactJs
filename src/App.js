f0d9c215a291c541bb1a0134ed2138e7b85118bf
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-11-08 21:09:54 +0000

    create a form for add  client

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 
+
 const App = () =>{
    // let hasCustumer = false
     const [items, setItems] = useState(['Eu','sei','eu','sei'])
@@ -23,6 +24,18 @@ const App = () =>{
             Clique aqui para cadastra um novo Cliente
             <br/>
             <button onClick={() => addItem('paranoia')}>+</button>
+        </div>
+    )
+    const formAddCostumer =(
+        <div>
+            <form onSubmit={this.renderAddCustomer} style={{display:'flex', width:'50vw', height:'50vh', 
+            backgroundColor:'black', opacity:50}}>
+                <label>Nome
+                <input type="text"/>
+                </label>
+                <br/>
+                <button type="submit">Enviar</button>
+            </form>
         </div>
     )
     return(
